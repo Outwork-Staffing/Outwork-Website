d69cd0cb81af59f384beed4836c7861704f4cd2c
bryan sanders <iambryansanders@gmail.com>
2023-12-08 23:30:18 -0500

    try this

--- resources/js/Blocks/Header.vue
+++ resources/js/Blocks/Header.vue
@@ -31,7 +31,7 @@
                 <div class=" flex items-center justify-between">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">Outwork Staffing</span>
-                        <img class="h-8 w-auto" src="logo.svg" alt="" />
+                        <img class="h-8 w-auto" src="@/logo.svg" alt="" />
                     </a>
 
                     <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
