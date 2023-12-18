9c7aa8b69561159952cfad28fef2bcba179ce126
bryan sanders <iambryansanders@gmail.com>
2023-12-18 14:51:57 -0500

    testing

--- resources/js/pages/404.vue
+++ resources/js/pages/404.vue
@@ -5,6 +5,17 @@ import Heading from '@/components/Heading.vue';
 import Button from '@/components/Button.vue';
 </script>
 <template>
-    123
+    <Main>
+        <Row class="pt-40 text-center min-h-screen">
+            <div class="grid gap-8">
+                <h1 class="text-9xl text-primary font-black">404</h1>
+                <Heading size="lg" class="font-bold mb-0">Can't find the page you're looking for.</Heading>
+                <div>
+                    <Button link="/" class="mt-8">Go Home</Button>
+                </div>
+
+            </div>
+        </Row>
+    </Main>
 </template>
 
