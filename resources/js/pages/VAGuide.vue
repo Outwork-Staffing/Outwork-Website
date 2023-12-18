f82dfc8b3a131d4ef7bca4a75ba25b66045f95b7
bryan sanders <iambryansanders@gmail.com>
2023-12-18 14:36:41 -0500

    fix it?

---
+++ resources/js/pages/VAGuide.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Main from '../layouts/Main.vue';
+import Row from '../components/Row.vue';
+import Heading from '../components/Heading.vue';
+import CTA from '../blocks/CTA.vue';
+import VAGuideContent from '../../md/VAGuideContent.md';
+</script>
+
+<template>
+    <Main>
+        <Row background="white" header="true">
+            <div class="mx-auto">
+                <Heading size="2xl" class="text-center text-heading">The Ultimate Guide to Virtual Assistants</Heading>
+                <Heading size="span" class="max-w-2xl mx-auto text-center">Expertise,
+                    efficiency, and
+                    cost-effective recruiting solutions as you grow your staff overseas.
+
+                </Heading>
+
+                <article class="my-12 max-w-5xl max-h-[96] mx-auto prose xl:prose-lg">
+                    <VAGuideContent />
+                </article>
+            </div>
+        </Row>
+        <CTA />
+
+    </Main>
+</template>
+
