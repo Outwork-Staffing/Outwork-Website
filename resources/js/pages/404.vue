1b4a621e16d202b0f3617d5760954539595e9ab8
bryan sanders <iambryansanders@gmail.com>
2023-12-18 15:18:39 -0500

    Update 404.vue

--- resources/js/pages/404.vue
+++ resources/js/pages/404.vue
@@ -1,9 +1,3 @@
-<script setup>
-import Main from '../layouts/Main.vue';
-import Row from '../components/Row.vue';
-import Heading from '../components/Heading.vue';
-import Button from '../components/Button.vue';
-</script>
 <template>
     <Main>
         <Row class="pt-40 text-center min-h-screen">
