bd205afbb0af3b9905be002a7a9c39c744bcebc4
bryan sanders <iambryansanders@gmail.com>
2023-12-18 14:40:18 -0500

    Update VAGuide.vue

--- resources/js/pages/VAGuide.vue
+++ resources/js/pages/VAGuide.vue
@@ -1,8 +1,8 @@
 <script setup>
-import Main from '../layouts/Main.vue';
-import Row from '../components/Row.vue';
-import Heading from '../components/Heading.vue';
-import CTA from '../blocks/CTA.vue';
+import Main from '@/layouts/Main.vue';
+import Row from '@/components/Row.vue';
+import Heading from '@/components/Heading.vue';
+import CTA from '@/blocks/CTA.vue';
 import VAGuideContent from '../../md/VAGuideContent.md';
 </script>
 
@@ -18,7 +18,6 @@ import VAGuideContent from '../../md/VAGuideContent.md';
                 </Heading>
 
                 <article class="my-12 max-w-5xl max-h-[96] mx-auto prose xl:prose-lg">
-                    <VAGuideContent />
                 </article>
             </div>
         </Row>
