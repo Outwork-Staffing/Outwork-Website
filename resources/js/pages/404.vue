c8ca022a1bf019412d08b937ef5e2db699d9a285
bryan sanders <iambryansanders@gmail.com>
2023-12-18 14:38:51 -0500

    dis

--- resources/js/pages/404.vue
+++ resources/js/pages/404.vue
@@ -1,8 +1,8 @@
 <script setup>
-import Main from '../layouts/Main.vue';
-import Row from '../components/Row.vue';
-import Heading from '../components/Heading.vue';
-import Button from '../components/Button.vue';
+import Main from '@/layouts/Main.vue';
+import Row from '@/components/Row.vue';
+import Heading from '@/components/Heading.vue';
+import Button from '@/components/Button.vue';
 </script>
 <template>
     <Main>
