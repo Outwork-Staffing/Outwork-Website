163b9b6f0282d77c0a2025dd1b1a5981b0d0d5e4
bryan sanders <iambryansanders@gmail.com>
2024-01-18 11:09:17 -0500

    keep it moving

--- resources/js/blocks/TestimonialLogoCloud.vue
+++ resources/js/blocks/TestimonialLogoCloud.vue
@@ -1,62 +1,31 @@
 <script setup>
+import InfiniteSlideBar from 'vue3-infinite-slide-bar';
+
 let logos = ['avanti.svg', 'primepay.png', 'swivl.svg', 'attune.svg', 'truesafe.webp', 'sparx.png', 'tcc.png', 'eight.svg', 'encore.png', 'insight.png', 'wil.webp'];
 </script>
 <template>
-    <div class="logos pt-8" :class="{ 'white-fade': fadeToWhite }">
-        <div class="logos-slide">
-            <div v-for="logo in logos" :key="logo" class="inline-block mr-8 mb-4 rounded-3xl">
-                <img :src="'/logos/' + logo" alt="Logo" class="block">
-            </div>
+    <infinite-slide-bar :barStyle="{ padding: '5px 0' }" duration="60s" class=" logos white-fade">
+        <div v-for="logo in logos" :key="logo" class="inline-block mr-8 mb-4 rounded-3xl logo-parent">
+            <img :src="'/logos/' + logo" alt="Logo" class="logo">
         </div>
-    </div>
+    </infinite-slide-bar>
 </template>
 
-<script>
-export default {
-    props: {
-        logos: Array,
-        fadeToWhite: {
-            type: Boolean,
-            default: false
-        }
-    },
-    mounted() {
-        var copy = document.querySelector(".logos-slide").cloneNode(true);
-        document.querySelector(".logos").appendChild(copy);
-    }
-};
-</script>
+<style>
+.logo {
+    max-height: 70px;
+    max-width: 120px;
+    height: auto;
+    width: auto;
+}
 
-<style scoped>
+.vifnslb-bar {
+    display: flex;
+    align-items: center;
+}
+
 .logos {
-    overflow: hidden;
-    white-space: nowrap;
     position: relative;
-}
-
-.logos-slide {
-    display: inline-block;
-    animation: slide 6s infinite linear;
-}
-
-@keyframes slide {
-    from {
-        transform: translateX(0);
-    }
-
-    to {
-        transform: translateX(-100%);
-    }
-}
-
-.logos:hover .logos-slide {
-    animation-play-state: paused;
-}
-
-@media (max-width: 768px) {
-    .logos-slide {
-        animation: slide 30s infinite linear;
-    }
 }
 
 .logos:before,
@@ -77,14 +46,6 @@ export default {
 .logos:after {
     right: 0;
     background: linear-gradient(to right, rgba(255, 255, 255, 0), rgb(255, 255, 255, 100));
-}
-
-@media (max-width: 768px) {
-
-    .logos:before,
-    .logos:after {
-        width: 33px;
-    }
 }
 
 .white-fade:before {
--- resources/js/pages/SuccessStories.vue
+++ resources/js/pages/SuccessStories.vue
@@ -13,13 +13,39 @@ const props = defineProps({
                 <Heading size="xl" class="text-center text-heading">Global Recruitment Success Stories</Heading>
                 <p class="text-xl max-w-2xl mx-auto text-center mx-12 text-body mb-6">Discover how companies are leveraging
                     global talent in major ways</p>
-
             </div>
-            <TestimonialLogoCloud :logos="logos" />
         </Row>
+        <div class="max-w-7xl mx-auto text-center">
+            <TestimonialLogoCloud :logos="logos" class="white-fade" />
+        </div>
+
         <Row>
-            //quote
-            quote
+            <Fifty imageLocation="left" imageMobile="false"
+                imageUrl="https://images.unsplash.com/photo-1545112969-bdd91ef544c7?q=80&w=4471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                altText="Men sitting around computer">
+                <template #right>
+                    <p class="text-xl text-left text-body mb-6 leading-8">"Outsourcing has been a
+                        game-changer for web SaaS, significantly boosting our profit margins and project speed remarkably.
+                        The
+                        exceptional talent
+                        and efficiency provided by Outwork Staffing have enabled us to deliver superior quality websites at
+                        an
+                        unprecedented pace, truly exceeding our clients' expectations."
+                    </p>
+                    <div class="relative flex items-center gap-x-4 mx-auto mb-6">
+                        <img src="https://ik.imagekit.io/jcj/paul_k47Jf9F7Iu.jpeg?updatedAt=1705593651175" alt=""
+                            class="h-10 w-10 rounded-full bg-gray-50">
+                        <div class="text-sm leading-6">
+                            <p class="font-semibold text-heading text-xl">
+                                <span class="absolute inset-0"></span>
+                                Paul Cox
+                            </p>
+                            <p class="text-gray-600">Founder, The Church Co</p>
+                        </div>
+                    </div>
+                    <Button>Open Full Case Study</Button>
+                </template>
+            </Fifty>
         </Row>
 
         <Row background="light">
