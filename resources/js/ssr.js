a03dec8f8a43f2dabf8b5be1a82dafa9e16d9dd1
bryan sanders <iambryansanders@gmail.com>
2024-04-05 15:22:46 -0400

    update page ressolver

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,16 +1,16 @@
+import { createSSRApp, h } from 'vue'
+import { renderToString } from '@vue/server-renderer'
 import { createInertiaApp } from '@inertiajs/vue3'
 import createServer from '@inertiajs/vue3/server'
-import { renderToString } from '@vue/server-renderer'
-import { createSSRApp, h } from 'vue'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = 'Outwork Staffing';
 
 createServer(page =>
     createInertiaApp({
         page,
         render: renderToString,
-        resolve: name => {
-            const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-            return pages[`./Pages/${name}.vue`]
-        },
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({
                 render: () => h(App, props),
