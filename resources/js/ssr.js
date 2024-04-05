12dcb55ab68ebe8e895e593c1f55cba9e0f9a5ad
bryan sanders <iambryansanders@gmail.com>
2024-04-05 16:14:49 -0400

    try this

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,6 +1,7 @@
 import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
 
 const appName = 'Outwork Staffing';
 
