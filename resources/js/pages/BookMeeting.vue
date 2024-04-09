b868b5a2397cf408f72ea0a7c815c7537b4d1722
bryan sanders <iambryansanders@gmail.com>
2024-04-09 10:43:30 -0400

    Book Meeting Redirect

--- resources/js/pages/BookMeeting.vue
+++ resources/js/pages/BookMeeting.vue
@@ -23,18 +23,15 @@ function initializeSavvyCal() {
     <Main>
         <Row class="pt-12 ">
             <Heading size="span" tag="h6">
-                contact us
+                Last Step
             </Heading>
-            <Heading size="2xl" tag="h2">
-                Ready to get started hiring?
+            <Heading size="xl" tag="h2">
+                Form Submitted - now let's schedule a call.
             </Heading>
-            <p class="body-paragraph max-w-4xl">Say goodbye to the stress of hiring global talent. Use the form below to
-                schedule a
-                call with our team and learn how we can help you hire the best talent. Our email is also below if you
-                would
-                prefer that.</p>
-            <Button type="text" link="mailto:operations@outworkstaffing.com">Email
-                operations@outworkstaffing.com</Button>
+            <p class="body-paragraph max-w-4xl">The next step in the process is to chat with our team about your needs.
+                We will review the information you submitted and prepare everything you may need on our first call.
+                <br><br> We will review timeline, pricing, your needs, and global payment compliance. </p>
+
             <div id="booking-page" class="my-12 min-h-screen">
             </div>
 
