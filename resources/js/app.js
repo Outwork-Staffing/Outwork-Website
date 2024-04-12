0067d070e0590350067d0ae45c6ed0fe8c9861c7
bryan sanders <iambryansanders@gmail.com>
2024-04-11 23:00:26 -0400

    try this

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-import { createSSRApp, h } from 'vue'
+import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import * as Sentry from "@sentry/vue";
 
@@ -8,25 +8,10 @@ createInertiaApp({
         return pages[`./pages/${name}.vue`]
     },
     setup({ el, App, props, plugin }) {
-        const app = createSSRApp({ render: () => h(App, props) })
+       createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
 
-        Sentry.init({
-            app,
-            dsn: "https://57066fdc4e56606d67809be51af503a4@o4507071142625280.ingest.us.sentry.io/4507071142952960",
-            integrations: [
-                Sentry.browserTracingIntegration(),
-                Sentry.replayIntegration(),
-            ],
-            // Performance Monitoring
-            tracesSampleRate: 1.0, //  Capture 100% of the transactions
-            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-            tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-            // Session Replay
-            replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-            replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-        });
-        return app;
+       
     },
 })
