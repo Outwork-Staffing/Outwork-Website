e0f1c9b64b6a4a2421bac7a0b3baec746d9a669d
bryan sanders <iambryansanders@gmail.com>
2024-05-09 11:42:21 -0400

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import { createApp, createSSRApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import Layout from './Layouts/Main.vue'
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
         let page = pages[`./pages/${name}.vue`]
-        page.default.layout = page.default.layout || Layout
         return page
     },
     setup({ el, App, props, plugin }) {
