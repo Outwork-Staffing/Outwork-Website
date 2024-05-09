3f9721fd620c37973767a7dd8fe86a4eeed565b6
bryan sanders <iambryansanders@gmail.com>
2024-05-09 11:41:44 -0400

    Revert "Update ssr.js"
    This reverts commit 8f641d0d0387244551c6927270611eb2b4a36895.

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -2,7 +2,6 @@ import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3';
-import Layout from "./Layouts/Main.vue";
 
 createServer(page =>
     createInertiaApp({
@@ -10,9 +9,7 @@ createServer(page =>
         render: renderToString,
         resolve: name => {
             const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
-            let page = pages[`./pages/${name}.vue`]
-            page.default.layout = page.default.layout || Layout
-            return page
+            return pages[`./pages/${name}.vue`]
         },
         setup({ App, props, plugin }) {
             return createSSRApp({
