6aecbfdc02def38f61993f8fcf412841ee416afd
bryan sanders <iambryansanders@gmail.com>
2024-05-09 10:21:09 -0400

    Add Meeting back

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -106,4 +106,4 @@ const props = defineProps({
     </Main>
 </template>
 
-  
+
--- resources/js/pages/StartHiring.vue
+++ resources/js/pages/StartHiring.vue
@@ -12,36 +12,32 @@
                 call with our team and learn how we can help you hire the best talent. Our email is also below if you would
                 prefer that.</p>
             <Button type="text" link="mailto:operations@outworkstaffing.com">Email operations@outworkstaffing.com</Button>
-            <iframe class="min-h-[500px]"
-                data-tally-src="https://tally.so/embed/w72xp2?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-                loading="lazy" width="100%" height="1" frameborder="0" marginheight="0" marginwidth="0"
-                title="Get Started Hiring"></iframe>
+            <div id="booking-page" class="my-12 min-h-screen">
+                <!-- this is where we will inject the interface -->
+            </div>
 
 
         </Row>
     </Main>
 </template>
 <script setup>
-import { onMounted } from 'vue';
-
-onMounted(() => {
-    const loadTallyEmbed = () => {
-        const embedScriptUrl = "https://tally.so/widgets/embed.js";
-        let script = document.querySelector(`script[src="${embedScriptUrl}"]`);
-
-        if (!script) {
-            script = document.createElement('script');
-            script.src = embedScriptUrl;
-            document.body.appendChild(script);
-        }
-
-        script.onload = () => {
-            if (typeof Tally !== 'undefined') {
-                Tally.loadEmbeds();
-            }
-        };
-    };
-
-    loadTallyEmbed();
+import { onBeforeMount, ref } from 'vue';
+const bookingPage = ref(null);
+onBeforeMount(() => {
+    loadSavvyCalScript();
 });
+function loadSavvyCalScript() {
+    const script = document.createElement('script');
+    script.src = 'https://embed.savvycal.com/v1/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+    script.onload = () => initializeSavvyCal();
+}
+function initializeSavvyCal() {
+    window.SavvyCal = window.SavvyCal || function () {
+        (window.SavvyCal.q = window.SavvyCal.q || []).push(arguments);
+    };
+    SavvyCal('init');
+    SavvyCal('inline', { link: 'bryan-outwork-staffing/inquiry', selector: '#booking-page' });
+}
 </script>
